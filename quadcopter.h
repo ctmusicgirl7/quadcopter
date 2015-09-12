e04b01a8e75004889d9b401dfe6401c1fe558fbf
ctmusicgirl7 <ctmusicgirl7@gmail.com>
2015-09-12 15:22:50 -0700

    Update quadcopter.h
    update

--- quadcopter.h
+++ quadcopter.h
@@ -3,17 +3,32 @@
 
 #include <stdint.h>
 
-// get the orientation: 0 -> flat, (-)127 -> max tilt
-int8_t getPitch(); // leaning forwards / backwards: positive -> forwards
-int8_t getRoll(); // leaning right / left: positive -> right
+/* get the orientation: 0 -> flat, (-)127 -> max tilt
+ */
+/* leaning forwards / backwards: positive -> forwards
+ */
+int8_t getPitch(); 
+/* leaning right / left: positive -> right
+ */
+ int8_t getRoll(); 
 
-// get the height: 0 -> on the ground, 255 -> 255 inches high
-uint8_t getHeight();
+/* get the height: 0 -> on the ground, 255 -> 255 inches high
+ */
+ uint8_t getHeight();
 
-// set the motor strength: 0 -> off, 255 -> max thrust
-void setFR(uint8_t); // front right
-void setFL(uint8_t); // front left
-void setBR(uint8_t); // back right
-void setBL(uint8_t); // back left
+/* set the motor strength: 0 -> off, 255 -> max thrust
+ */
+/* front right
+ */
+void setFR(uint8_t); 
+/* front left
+ */
+void setFL(uint8_t); 
+/* back right
+ */
+void setBR(uint8_t); 
+/* back left
+ */
+void setBL(uint8_t); 
 
 #endif
