8c86907ba40af37981d997ac29b39def18b7bae1
ctmusicgirl7 <ctmusicgirl7@gmail.com>
2015-09-13 17:25:53 -0700

    Update quadcopter.c

--- quadcopter.c
+++ quadcopter.c
@@ -1,4 +1,4 @@
-#include <quadcopter.h>
+#include "quadcopter.h"
 
 /* get the orientation: 0 -> flat, (-)127 -> max tilt
  */
@@ -6,106 +6,106 @@
  */
 int8_t getPitch()
 {
-    printf("getPitch"); 
+    printf("getPitch");
 }
 
 /* leaning right / left: positive -> right
  */
 int8_t getRoll()
 {
-    printf("getRoll"); 
-} 
+    printf("getRoll");
+}
 
 /* get the height: 0 -> on the ground, 255 -> 255 inches high
  */
 uint8_t getHeight()
 {
-    printf("getHeight"); 
+    printf("getHeight");
 }
 
 /* set the motor strength: 0 -> off, 255 -> max thrust
  */
 /* front right
  */
-void setFR(uint8_t)
+void setFR(uint8_t fr)
 {
-    printf("setFr"); 
+    printf("setFr");
 }
 
 /* front left
  */
-void setFL(uint8_t)
+void setFL(uint8_t fl)
 {
-    printf("setFl"); 
-} 
+    printf("setFl");
+}
 
 /* back right
  */
-void setBR(uint8_t)
+void setBR(uint8_t br)
 {
     printf("setBr"); 
 } 
 
 /* back left
  */
-void setBL(uint8_t)
+void setBL(uint8_t bl)
 {
     printf("setBl"); 
 }
 
 /* run quadcopter
  */
-void run(struct quadcopter quad)
+void run(quad qd)
 {
     printf("Attention: run quadcopter!");
 }
 
 int main()
 {
-    quad = {0};
-	run(quad);
+    quad qd = {0, 0, 0, 0, 0, 0};
+    run(qd);
     while (1)
-	{
-	    /* adjust height */
-        quad.height = getHeight();
-		if(quad.height != HEIGHT)
-		{
-		    quad.height = quad.height < HEIGHT ? (quad.height + 1) : (quad.height - 1);
-		}
-	    /* adjust pitch */
-        quad.pitch = getPitch();
-		if(quad.pitch != 0)
-		{
-		    quad.pitch = quad.pitch < 0 ? (quad.pitch + 1) : (quad.pitch - 1);
-		}
-	    /* adjust roll */
-        quad.roll = getRoll();
-		if(quad.roll != 0)
-		{
-		    quad.roll = quad.roll < 0 ? (quad.roll + 1) : (quad.roll - 1);
-		}
-		if(quad.fr != MOTOR)
-		{
-		    quad.fr = quad.fr < MOTOR ? (quad.fr + 1) : (quad.fr - 1);
-			setFr(quad.fr);
-		} 
-		if(quad.fl != MOTOR)
-		{
-		    quad.fl = quad.fl < MOTOR ? (quad.fl + 1) : (quad.fl - 1);
-			setFl(quad.fl);
-		} 
-		if(quad.br != MOTOR)
-		{
-		    quad.br = quad.br < MOTOR ? (quad.br + 1) : (quad.br - 1);
-			setBr(quad.br);
-		} 
-		if(quad.fr != MOTOR)
-		{
-		    quad.bl = quad.bl < MOTOR ? (quad.bl + 1) : (quad.bl - 1);
-			setBl(quad.bl);
-		} 
-		run(quad);
-	}
+    {
+        /* adjust height */
+        qd.height = getHeight();
+        if(qd.height != HEIGHT)
+        {
+                qd.height = qd.height < HEIGHT ? (qd.height + 1) : (qd.height - 1);
+        }
+        /* adjust pitch */
+        qd.pitch = getPitch();
+        if(qd.pitch != 0)
+        {
+            qd.pitch = qd.pitch < 0 ? (qd.pitch + 1) : (qd.pitch - 1);
+        }
+        /* adjust roll */
+        qd.roll = getRoll();
+        if(qd.roll != 0)
+        {
+            qd.roll = qd.roll < 0 ? (qd.roll + 1) : (qd.roll - 1);
+        }
+        if(qd.fr != MOTOR)
+        {
+             qd.fr = qd.fr < MOTOR ? (qd.fr + 1) : (qd.fr - 1);
+             setFr(qd.fr);
+        }
+        if(qd.fl != MOTOR)
+        {
+             qd.fl = qd.fl < MOTOR ? (qd.fl + 1) : (qd.fl - 1);
+             setFl(qd.fl);
+        }
+        if(qd.br != MOTOR)
+        {
+             qd.br = qd.br < MOTOR ? (qd.br + 1) : (qd.br - 1);
+             setBr(qd.br);
+        }
+        if(qd.fr != MOTOR)
+        {
+             qd.bl = qd.bl < MOTOR ? (qd.bl + 1) : (qd.bl - 1);
+             setBl(qd.bl);
+        }
+        run(qd);
+   }
 
    return 0;
 }
